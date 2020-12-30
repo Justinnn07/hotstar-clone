76b8c26a028730170b81d97739e2214f4e81c6a2
Justinnn07 <justinv2006@gmail.com>
2020-12-30 20:39:00 +0530

    added one more sec :fire:

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import Header from "./Header";
+import Slides from "./Slides";
 
 const App = () => {
   return (
     <div className="app">
       <Header />
+      <Slides />
     </div>
   );
 };
