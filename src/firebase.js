f01af999e4b057a45e7bfe023bac683250327bce
Justinnn07 <justinv2006@gmail.com>
2020-12-31 14:48:14 +0530

    added firebase hosting :fire:

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,13 @@
 import firebase from "firebase";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyA3HYlAPUuODQRtejheXPuGVMzuYc0ORn0",
-  authDomain: "hotstar-clone-3011.firebaseapp.com",
-  projectId: "hotstar-clone-3011",
-  storageBucket: "hotstar-clone-3011.appspot.com",
-  messagingSenderId: "249114476785",
-  appId: "1:249114476785:web:5eb19e61fc17e764de6950",
-  measurementId: "G-QKQNZJ7N39",
+  apiKey: "AIzaSyA2p8nwK8i3UeiDqpUz3OHrk54XEhef6ys",
+  authDomain: "hotstar-clone-2109.firebaseapp.com",
+  projectId: "hotstar-clone-2109",
+  storageBucket: "hotstar-clone-2109.appspot.com",
+  messagingSenderId: "383086856967",
+  appId: "1:383086856967:web:aa9b608f6faa8aa8c293a2",
+  measurementId: "G-CW8HXGZ2RQ"
 };
 
 const firebaseapp = firebase.initializeApp(firebaseConfig);
